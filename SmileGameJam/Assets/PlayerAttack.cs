b0623587a4bf342e7e1afd276a7a15efad018679
= <=>
2019-04-06 03:04:18 +0900

    Move

---
+++ SmileGameJam/Assets/PlayerAttack.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+
+    [Header("Skill")]
+    public SkillBase nowSkill;
+
+    [Header("Ultimate")]
+    public UltBase nowUltimate;
+
+    private int count = 0;
+    public int nowUltCount
+    {
+        get
+        {
+            return count;
+        }
+        set
+        {
+            count = Mathf.Clamp(value, 0 , maxUltCount);
+        }
+    }
+    public int maxUltCount = 3;
+
+    public void UseSkill()
+    {
+        nowSkill.UseSkill(ChargeUltimate);
+    }
+
+    public void ChargeUltimate()
+    {
+        nowUltCount += 1;
+    }
+
+    public void UseUltimate()
+    {
+        if (maxUltCount > nowUltCount)
+        {
+            nowUltCount = 0;
+            nowUltimate.UseUltimate();
+        }
+    }
+}
