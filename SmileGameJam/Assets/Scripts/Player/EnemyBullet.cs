2fc9b382aa0cb8692318ecf4dc609deb5407d15a
= <=>
2019-04-06 20:16:14 +0900

    AI

---
+++ SmileGameJam/Assets/Scripts/Player/EnemyBullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour {
+
+    public float damage = 5;
+    public float speed = 12;
+    public float range;
+
+	void Start () {
+        range = 6 + Random.Range(-0.3f, 0.2f);
+        speed = 12 + Random.Range(-1.5f, 1.5f);
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        range -= speed * Time.deltaTime;
+        if (range <= 0)
+            Destroy(gameObject);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<UnitInfo>().healthPoint -= damage;
+            Destroy(gameObject);
+        }
+    }
+}
--- SmileGameJam/Assets/Scripts/test/TestMoving.cs
+++ SmileGameJam/Assets/Scripts/test/TestMoving.cs
@@ -5,54 +5,157 @@ using UnityEngine.AI;
 
 public class TestMoving : MonoBehaviour
 {
+    public enum State { Wander, Chase, Run }
+    public State nowState;
+
+    public Vector3 sight;
+
     public float speed = 5.0f;
+    private float playerGap;
+    public float delay = 0;
+    public bool isWait = false;
     NavMeshAgent agent;
 
-    public Transform target;
+    public GameObject bullet;
 
-    public GameObject player;
-    public GameObject spawnPoint;
-    public GameObject[] points = new GameObject[9];
+    public Transform target = null;
+    public Transform player;
 
-    float mindist = 99999.0f;
-    float dist = 99999.0f;
+    public Transform[] wayPoints;
 
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
-        spawnPoint = GameObject.FindGameObjectWithTag("SpawnPoint");
-
         agent = GetComponent<NavMeshAgent>();
         agent.speed = speed;
 
-        target = player.transform;
+        playerGap = GetComponent<UnitInfo>().stopDist;
+
+        target = wayPoints[Random.Range(0, wayPoints.Length - 1)];
+
+        StartCoroutine("ResetDestination", 0.03f);
     }
 
-    private void CheckItem()
+    private void ReChase()
     {
-
+        isWait = false;
     }
 
     void Update()
     {
-        mindist = Mathf.Abs(Vector3.Distance(transform.position, player.transform.position));
-        for (int i = 0; i < spawnPoint.transform.childCount; i++)
+        float minDistSqr = 0;
+        Collider nearestTarget = null;
+        bool flag = false;
+
+        if (isWait)
         {
-            if (spawnPoint.transform.GetChild(i).GetComponent<CheckItem>().checkItem)
+            nowState = State.Wander;
+            if (nowState != State.Wander)
+                target = wayPoints[Random.Range(0, wayPoints.Length - 1)];
+            if (Vector3.SqrMagnitude(transform.position - target.position) < 0.2f)
+                target = wayPoints[Random.Range(0, wayPoints.Length - 1)];
+            Invoke("ReChase", 5f);
+            return;
+        }
+
+        Collider[] cols = Physics.OverlapBox(transform.position + Vector3.up * sight.y * 0.5f, sight);
+        foreach(Collider col in cols)
+        {
+            if (col.CompareTag("Player"))
             {
-                dist = Mathf.Abs(Vector3.Distance(transform.position, spawnPoint.transform.GetChild(i).GetComponent<CheckItem>().ReturnItem().position));
-                if (mindist > dist)
+                if (Random.Range(0, 100) < 20)
                 {
-                    mindist = dist;
-                    target = spawnPoint.transform.GetChild(i).GetComponent<CheckItem>().ReturnItem();
+                    nowState = State.Chase;
+                    target = col.transform;
+                    agent.stoppingDistance = playerGap;
+                    flag = true;
+                    break;
+                }
+            }
+            else if (col.CompareTag("Item"))
+            {
+                if (nearestTarget == null)
+                {
+                    nearestTarget = col;
+                    minDistSqr = Vector3.SqrMagnitude(transform.position - col.transform.position);
+                }
+                else if (minDistSqr > Vector3.SqrMagnitude(transform.position - col.transform.position))
+                {
+                    nearestTarget = col;
+                    minDistSqr = Vector3.SqrMagnitude(transform.position - col.transform.position);
                 }
             }
         }
-        if (target == player.transform)
-            agent.stoppingDistance = gameObject.GetComponent<UnitInfo>().stopDist;
-        else
-            agent.stoppingDistance = 0;
-        agent.SetDestination(target.position);
+        if (!flag)
+        {
+            if (nearestTarget == null)
+            {
+                nowState = State.Wander;
+                if (nowState != State.Wander)
+                    target = wayPoints[Random.Range(0, wayPoints.Length - 1)];
+                else if(Vector3.SqrMagnitude(transform.position - target.position) < 0.2f)
+                    target = wayPoints[Random.Range(0, wayPoints.Length - 1)];
+            }
+            else
+            {
+                nowState = State.Chase;
+                target = nearestTarget.transform;
+                agent.stoppingDistance = 0;
+            }
+        }
+
+        if (target == player && delay <= 0)
+        {
+            delay = 3;
+            StartCoroutine(UseSkill(Random.Range(0, 3), 6, transform.position + Vector3.up * 0.3f, transform.eulerAngles.y));
+            if (Random.Range(0, 100) < 20)
+                isWait = true;
+        }
+
+        if (delay > 0)
+            delay -= Time.deltaTime;
     }
 
+    IEnumerator UseSkill(int power, float range, Vector3 position, float rotation)
+    {
+        float startAngle;
+        float angleGap;
+        agent.speed = 0;
+        if (power == 0)
+        {
+            yield return new WaitForSeconds(0.5f);
+            delay = 4;
+            startAngle = rotation - 12;
+            angleGap = 4;
+        }
+        else
+        {
+            yield return new WaitForSeconds(0.35f);
+            delay = 5f;
+            startAngle = rotation - 10;
+            angleGap = 3.5f;
+        }
+
+        for (int i = 0; i < 15; i++)
+            Instantiate(bullet, position, Quaternion.Euler(0, startAngle + angleGap * i + Random.Range(-0.1f, 0.1f), 0));
+        agent.speed = 2.5f;
+    }
+
+    public IEnumerator ResetDestination(float term)
+    {
+        while (true)
+        {
+            if (target)
+                agent.SetDestination(target.position);
+            yield return new WaitForSeconds(term);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position + Vector3.up * sight.y * 0.5f, sight * 2);
+
+        foreach(Transform point in wayPoints)
+            Gizmos.DrawSphere(point.position, 0.5f);
+    }
 }
