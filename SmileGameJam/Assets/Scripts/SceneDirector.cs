d441b0bd5e89e5206bd7bc3df0faa76c4384f2ae
= <=>
2019-04-06 17:58:20 +0900

    WaitScene

--- SmileGameJam/Assets/Scripts/SceneDirector.cs
+++ SmileGameJam/Assets/Scripts/SceneDirector.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneDirector : MonoBehaviour {
-
-    public GameObject startScene;
-    public GameObject IngameScene;
-
-    public void GoMainScene()
-    {
-        startScene.SetActive(false);
-        IngameScene.SetActive(true);
-    }
     
     public void GoScene(string sceneName)
     {
