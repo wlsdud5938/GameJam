ed00a0e77afb363c75ea74139102aff72a44265f
= <=>
2019-04-07 01:38:59 +0900

    Name

---
+++ SmileGameJam/Assets/Scripts/StartManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartManager : MonoBehaviour {
+
+    public Text nicknameText; 
+
+    public void Start()
+    {
+        nicknameText.text = PlayerPrefs.GetString("Name");
+    }
+}
---
+++ SmileGameJam/Assets/Scripts/WaitManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class WaitManager : MonoBehaviour {
+
+    public GameObject NameInputFiled;
+    public GameObject clickText;
+
+    public InputField nicknameText;
+
+    public SceneDirector sceneDirector;
+
+    public void ClickToStart()
+    {
+        NameInputFiled.SetActive(true);
+        clickText.SetActive(false);
+        nicknameText.text = PlayerPrefs.GetString("Name", string.Empty);
+    }
+
+    public void EnterName(string name)
+    {
+        PlayerPrefs.SetString("Name", name);
+        sceneDirector.GoScene("StartScene");
+    }
+}
