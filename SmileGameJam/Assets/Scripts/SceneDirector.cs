f8e37d1f83114ac8888f4ef16b66747101cd9708
= <=>
2019-04-06 17:51:52 +0900

    SceneManager

---
+++ SmileGameJam/Assets/Scripts/SceneDirector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneDirector : MonoBehaviour {
+
+    public GameObject startScene;
+    public GameObject IngameScene;
+
+    public void GoMainScene()
+    {
+        startScene.SetActive(false);
+        IngameScene.SetActive(true);
+    }
+    
+    public void GoScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
