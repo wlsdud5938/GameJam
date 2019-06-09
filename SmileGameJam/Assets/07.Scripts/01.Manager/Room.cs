2d90562eb9c73fc740a00aaa897f93d43329fdc9
= <=>
2019-06-09 18:15:57 +0900

    Pawn

--- SmileGameJam/Assets/07.Scripts/01.Manager/Room.cs
+++ SmileGameJam/Assets/07.Scripts/01.Manager/Room.cs
@@ -16,24 +16,6 @@ public class Room : MonoBehaviour
 
     private int obstacleCount;
 
-    [Header("[Grid]")]
-    public LayerMask unwalkableMask;    //장애물 레이어 마스크
-    LayerMask walkableMask;
-    public Vector2 gridWorldSize;       //맵 사이즈
-    public int obstacleProximityPenalty = 10;   //장애물 우선 패널티
-    public float nodeRadius;            //노드 크기
-    float nodeDiameter; //지름
-    int gridSizeX, gridSizeY;
-    Node[,] grid;
-
-    public int MaxSize
-    {
-        get
-        {
-            return gridSizeX * gridSizeY;
-        }
-    }
-
     public void Open(int r)
     {
         enterances[r].SetActive(true);
@@ -109,86 +91,4 @@ public class Room : MonoBehaviour
         }
         Debug.Log("ClearTheRoom");
     }
-
-
-    void CreateGrid()
-    {
-        grid = new Node[gridSizeX, gridSizeY];
-        //좌측 하단
-        Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
-
-        for (int x = 0; x < gridSizeX; x++)
-        {
-            for (int y = 0; y < gridSizeY; y++)
-            {
-                //해당 노드 (위치 * 노드 지름 * 노드 반지름 => x * 1 + 0.5)
-                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
-                bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
-
-                int movementPenalty = 0;
-
-                Ray ray = new Ray(worldPoint + Vector3.up * 50, Vector3.down);
-                RaycastHit hit;
-                
-                if (Physics.Raycast(ray, out hit, 100, unwalkableMask)) movementPenalty = obstacleProximityPenalty;
-                else movementPenalty = 0;
-
-                Debug.Log(movementPenalty);
-                grid[x, y] = new Node(walkable, worldPoint, x, y, movementPenalty);
-            }
-        }
-    }
-
-    public List<Node> GetNeighbours(Node node)
-    {
-        List<Node> neighbours = new List<Node>();
-
-        for (int x = -1; x <= 1; x++)
-        {
-            for (int y = -1; y <= 1; y++)
-            {
-                if (x == 0 && y == 0)
-                    continue;
-
-                int checkX = node.gridX + x;
-                int checkY = node.gridY + y;
-
-                if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY)
-                {
-                    neighbours.Add(grid[checkX, checkY]);
-                }
-            }
-        }
-        return neighbours;
-    }
-
-    public Node NodeFromWorldPoint(Vector3 worldPosition)
-    {
-        float percentX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
-        float percentY = (worldPosition.z + gridWorldSize.y / 2) / gridWorldSize.y;
-        percentX = Mathf.Clamp01(percentX);
-        percentY = Mathf.Clamp01(percentY);
-
-        int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
-        int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
-        if (grid == null)
-            return null;
-        return grid[x, y];
-    }
-
-    //void OnDrawGizmos()
-    //{
-    //    Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
-
-    //    if (grid != null)
-    //    {
-    //        foreach (Node n in grid)
-    //        {
-    //            Gizmos.color = Color.Lerp(Color.white, Color.black, Mathf.InverseLerp(0, obstacleProximityPenalty, n.movementPenalty));
-
-    //            Gizmos.color = (n.walkable) ? Gizmos.color : Color.red;
-    //            Gizmos.DrawCube(n.worldPosition, Vector3.one * nodeDiameter);
-    //        }
-    //    }
-    //}
 }
--- SmileGameJam/Assets/07.Scripts/04.Monster/Pawn.cs
+++ SmileGameJam/Assets/07.Scripts/04.Monster/Pawn.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class Pawn : Monster
 {
+    public int stopDist = 2;
+
     public override void AttackPattern()
     {
         StartCoroutine(AttackAni());
@@ -29,15 +31,15 @@ public class Pawn : Monster
         float minDist = float.MaxValue;
         for (int i = 0; i < 4; i++)
         {
-            Ray ray = new Ray(transform.position + Vector3.up * 5, Vector3.down);
+            Vector3 checkPos = transform.position + Quaternion.Euler(0, 90 * i, 0) * Vector3.forward;
+            Ray ray = new Ray(checkPos + Vector3.up * 5, Vector3.down);
             if (!Physics.Raycast(ray, 100, unwalkableMask))
             {
-                float dist = Vector3.SqrMagnitude(target.position - transform.position + Quaternion.Euler(0, 90 * i, 0) * Vector3.forward);
+                float dist = Vector3.SqrMagnitude(target.position - checkPos);
                 if (dist < minDist)
                 {
                     minDist = dist;
                     dir = i;
-                    Debug.Log(i);
                 }
             }
         }
@@ -56,7 +58,7 @@ public class Pawn : Monster
 
             for (float i = 0; i < 1; i += Time.deltaTime * jumpSpeed)
             {
-                transform.position = new Vector3(originPos.x, Mathf.Sin(Mathf.PI * i) * 1.5f, originPos.z) + Quaternion.Euler(0, 90 * i, 0) * Vector3.forward * i;
+                transform.position = new Vector3(originPos.x, Mathf.Sin(Mathf.PI * i) * 1.5f, originPos.z) + Quaternion.Euler(0, 90 * dir, 0) * Vector3.forward * i;
                 yield return null;
             }
         }
