b324f2a5dd0a257fe779806e1b0a9f5d21035ca4
= <=>
2019-06-09 16:32:35 +0900

    Update Pawn.cs

--- SmileGameJam/Assets/07.Scripts/04.Monster/Pawn.cs
+++ SmileGameJam/Assets/07.Scripts/04.Monster/Pawn.cs
@@ -10,12 +10,12 @@ public class Pawn : Monster
 
     public override void MovePattern()
     {
-        StartCoroutine(MoveAni(Vector3.zero));
+        StartCoroutine(MoveAni());
     }
 
     public IEnumerator AttackAni()
     {
-        yield return StartCoroutine(MoveAni(Vector3.zero));
+        yield return StartCoroutine(MoveAni());
 
         Vector3 position = transform.position + Vector3.up;
         Quaternion rotation = Quaternion.LookRotation(target.position - transform.position);
@@ -23,33 +23,42 @@ public class Pawn : Monster
         newBullet.SetInformation(null, attack, bulletSpeed, 30);
     }
 
-    public IEnumerator MoveAni(Vector3 direction)
+    public IEnumerator MoveAni()
     {
         int dir = -1;
         float minDist = float.MaxValue;
-        for(int i = 0; i < 4; i++)
+        for (int i = 0; i < 4; i++)
         {
             Ray ray = new Ray(transform.position + Vector3.up * 5, Vector3.down);
             if (!Physics.Raycast(ray, 100, unwalkableMask))
             {
-                float dist = Vector3.Distance();
+                float dist = Vector3.SqrMagnitude(target.position - transform.position + Quaternion.Euler(0, 90 * i, 0) * Vector3.forward);
+                if (dist < minDist)
+                {
+                    minDist = dist;
+                    dir = i;
+                    Debug.Log(i);
+                }
             }
         }
 
-        float jumpSpeed = 3.0f;
-        Vector3 originPos = transform.position;
-
-        for (float i = 0; i < 1; i += Time.deltaTime * jumpSpeed)
+        if (dir != -1)
         {
-            transform.localScale = new Vector3(1 + i * 0.15f, 1 - i * 0.2f, 1 + i * 0.15f);
-            yield return null;
-        }
-        transform.localScale = Vector3.one;
+            float jumpSpeed = 3.0f;
+            Vector3 originPos = transform.position;
 
-        for (float i = 0; i < 1; i += Time.deltaTime * jumpSpeed)
-        {
-            transform.position = new Vector3(originPos.x, Mathf.Sin(Mathf.PI * i) * 1.5f, originPos.z) + direction * i;
-            yield return null;
+            for (float i = 0; i < 1; i += Time.deltaTime * jumpSpeed)
+            {
+                transform.localScale = new Vector3(1 + i * 0.15f, 1 - i * 0.2f, 1 + i * 0.15f);
+                yield return null;
+            }
+            transform.localScale = Vector3.one;
+
+            for (float i = 0; i < 1; i += Time.deltaTime * jumpSpeed)
+            {
+                transform.position = new Vector3(originPos.x, Mathf.Sin(Mathf.PI * i) * 1.5f, originPos.z) + Quaternion.Euler(0, 90 * i, 0) * Vector3.forward * i;
+                yield return null;
+            }
         }
     }
 }
